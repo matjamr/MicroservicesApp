1317462e19793d60b1654adc3b5d19693e83153f
Mateusz Jamróz <jamr.mat@gmail.com>
2022-06-27 17:26:00 +0200

    Init

---
+++ src/main/java/com/jamroz/microservicesapp/MicroservicesAppApplication.java
@@ -0,0 +1,13 @@
+package com.jamroz.microservicesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamroz/microservicesapp/MicroservicesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamroz.microservicesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
